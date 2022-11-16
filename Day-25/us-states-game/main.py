2d5a72421b3d65b04be25e71ddd1b7400cdc03ba
garretgvc <christiansongg@gmail.com>
2022-11-16 12:18:01 -0500

    edit with list comprehension

--- Day-25/us-states-game/main.py
+++ Day-25/us-states-game/main.py
@@ -16,10 +16,11 @@ while len(guessed_states) < 50:
                                     prompt="What's another State's "
                                                                                              "name?").title()
     if answer_state == "Exit":
-        missing_states = []
-        for state in state_names:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in state_names if state not in guessed_states]
+    # alternative code
+    #     for state in state_names:
+    #         if state not in guessed_states:
+    #             missing_states.append(state)
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
